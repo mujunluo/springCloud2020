d8935dcccaac07ca84bd7ff612b6407347a09301
mujunluo <15120023427@163.com>
2020-06-27 09:58:21 +0800

    111

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud2020/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud2020/EurekaMain7001.java
@@ -11,10 +11,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaMain7001 {
     public static void main(String[] args) {
-
         SpringApplication.run(EurekaMain7001.class,args);
     }
-    // 创建 一个dev分支测试
-    // 在dev的基础上创   建测试分支  test
-    // 7777777
 }
